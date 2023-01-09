76e28729be2a79e5f1ecb742f24f014a09122df7
vitordev <vitor.mancio@gmail.com>
2023-01-09 12:29:25 -0300

    create a config to NextUi on Next.js components

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
+import { NextUIProvider } from '@nextui-org/react'
+
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <NextUIProvider>
+    <Component {...pageProps} />
+    </NextUIProvider>
+    )
 }
--- pages/_document.js
+++ pages/_document.js
@@ -1,13 +1,14 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from '@nextui-org/react'
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
+        <Head>{CssBaseline.flush()}</Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
   )
 }
